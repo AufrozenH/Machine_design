275ad09d8b17b88c655671456bd53561d0e0de00
AufrozenH <2281036574@qq.com>
2024-04-01 12:58:48 +0800

    master

--- Machine_design/Core/Src/usart.c
+++ Machine_design/Core/Src/usart.c
@@ -22,13 +22,13 @@
 
 /* USER CODE BEGIN 0 */
 #include <stdio.h>
-#ifndef __GNUC__		// 如果不使用ARM CC V6编译�?
+#ifndef __GNUC__		// 如果不使用ARM CC V6编译器
 #pragma import(__use_no_semihosting)  // 不使用半主机函数
 struct __FILE{int handle;};	// 标准库需要的支持函数
-void _sys_exit(int x) {x = x;}	// 避免使用半主机模�?
+void _sys_exit(int x) {x = x;}	// 避免使用半主机模式
 #endif
 
-FILE __stdout; 			// ARM CC V6版本�?要添加支�?
+FILE __stdout; 			// ARM CC V6版本需要添加支持
 int fputc(int ch, FILE *f) {
 	HAL_UART_Transmit(&huart1 , (uint8_t *)&ch, 1, 0xFFFF);
 	return(ch);
